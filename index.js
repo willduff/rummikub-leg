d6a37962b7e1bfc09803d30260c98a5370188276
Will Duff <widuff@microsoft.com>
2024-12-27 10:27:04 -0800

    Rounded cuboid

--- index.js
+++ index.js
@@ -15,16 +15,17 @@ const main = (params) => {
     roundRadius: params.roundRadius,
     segments: params.segments
   })
-  return list
+  return straightSection
 }
 
 const getParameterDefinitions = () => {
     return [
-        { name: 'straightWidth',  type: 'int', initial: 150, caption: 'Straight Section: Width in mm' },
-        { name: 'straightDepth',  type: 'int', initial: 100, caption: 'Straight Section: Depth in mm' },
-        { name: 'straightHeight', type: 'int', initial: 100, caption: 'Straight Section: Height in mm' },
-        { name: 'roundRadius',    type: 'int', initial: 100, caption: 'Straight Section: Radius of extrusion in mm' },
-        { name: 'segments',       type: 'int', initial: 100, caption: 'Straight Section: Number of segments used to round extrusion' },
+        { name: 'straightWidth',  type: 'int',    initial: 50,   caption: 'Straight Section: Width in mm' },
+        { name: 'straightDepth',  type: 'int',    initial: 6.5,  caption: 'Straight Section: Depth in mm' },
+        { name: 'straightHeight', type: 'int',    initial: 2,    caption: 'Straight Section: Height in mm' },
+        { name: 'roundRadius',    type: 'number', initial: 0.96, caption: 'Straight Section: Radius of extrusion in mm' },
+        { name: 'segments',       type: 'int',    initial: 32,   caption: 'Straight Section: Number of segments used to round extrusion' },
+
         { name: 'angle', type: 'int', initial: 100, caption: 'Angled Section: Angle in ??' },
     ]
 }
