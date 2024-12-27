5126f7b874ffeae5ec683d902958e467abdb73ee
Will Duff <widuff@microsoft.com>
2024-12-27 11:51:26 -0800

    First try

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const { cube, sphere, roundedCuboid } = require('@jscad/modeling').primitives
-const { rotateX } = require('@jscad/modeling').transforms
+const { rotateX, rotateY, translate } = require('@jscad/modeling').transforms
 const { degToRad } = require('@jscad/modeling').utils
+const { union } = require('@jscad/modeling').booleans
  
 const rummikubLegSection = (params) => {
-  return [
+  return union(
     roundedCuboid(params),
     rotateX(degToRad(90), roundedCuboid(params)),
-  ]
+  )
+}
+
+const rummikubAngledSection = (params) => {
+  return translate(params.translatedCenter, 
+    rotateY(degToRad(params.angle), rummikubLegSection(params))
+  )
 }
 
 const main = (params) => {
@@ -16,18 +23,30 @@ const main = (params) => {
     roundRadius: params.roundRadius,
     segments: params.segments
   })
-  return straightSection
+  let angledSection = rummikubAngledSection({
+    size: [params.angledWidth, params.straightDepth, params.straightHeight],
+    center: [0, 0, 0],
+    roundRadius: params.roundRadius,
+    segments: params.segments,
+    angle: params.angle,
+    translatedCenter: [(params.straightWidth / 2) - params.angledSectionOverlap + (params.angledWidth / 2) -  params.angledSectionOverlap, 0, params.angledHeightOffset]
+  })
+  return union(straightSection, angledSection)
 }
 
 const getParameterDefinitions = () => {
     return [
-        { name: 'straightWidth',  type: 'number', initial: 10,   caption: 'Straight Section: Width in mm' },
-        { name: 'straightDepth',  type: 'number', initial: 7.5,  caption: 'Straight Section: Depth in mm' },
-        { name: 'straightHeight', type: 'number', initial: 2.5,    caption: 'Straight Section: Height in mm' },
-        { name: 'roundRadius',    type: 'number', initial: 1.2, caption: 'Straight Section: Radius of extrusion in mm' },
+        { name: 'straightWidth',  type: 'number', initial: 58,   caption: 'Straight Section: Width in mm' },
+        { name: 'straightDepth',  type: 'number', initial: 8,    caption: 'Straight Section: Depth in mm' },
+        { name: 'straightHeight', type: 'number', initial: 2.5,  caption: 'Straight Section: Height in mm' },
+
+        { name: 'roundRadius',    type: 'number', initial: 1.2,  caption: 'Straight Section: Radius of extrusion in mm' },
         { name: 'segments',       type: 'int',    initial: 16,   caption: 'Straight Section: Number of segments used to round extrusion' },
 
-        { name: 'angle', type: 'int', initial: 100, caption: 'Angled Section: Angle in ??' },
+        { name: 'angle',                type: 'number', initial: 30, caption: 'Angled Section: Angle in degrees' },
+        { name: 'angledWidth',          type: 'number', initial: 12, caption: 'Angled Section: Width in mm' },
+        { name: 'angledHeightOffset',   type: 'number', initial: -2.25,  caption: 'Angled Section: Height offset of the angled section in mm' },
+        { name: 'angledSectionOverlap', type: 'number', initial: 2,  caption: 'Angled Section: Overlap straight and angled section in mm' },
     ]
 }
 
